c73af51b16a41039f9d071096f7d62f2e5087dc8
rafa <rafa@rafa>
2016-08-08 15:44:23 +0200

    Contenido y envío de email

--- src/java/utilidades/Constantes.java
+++ src/java/utilidades/Constantes.java
@@ -262,7 +262,6 @@ public class Constantes {
                     + "<li> Password: " + pass + "</li>"
                     + "<br />If you want to change your data has to access the application settings";
         }
-
         cad += "<br />Go to this url and login with your credentials <a href=\"https://moleqla.upo.es/web\">MoleQla</a>";
         return cad;
     }
