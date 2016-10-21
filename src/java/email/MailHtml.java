8d1bed46df00342e6d07ff768996974d2073b1ea
rafa <rafa@rafa>
2016-10-21 20:03:28 +0200

    Configuración email

--- src/java/email/MailHtml.java
+++ src/java/email/MailHtml.java
@@ -80,8 +80,8 @@ public class MailHtml {
         Properties props = new Properties();
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.host", "mail.upo.es");
+        props.put("mail.smtp.port", "25");
 
         Session session = Session.getInstance(props,
           new javax.mail.Authenticator() {
